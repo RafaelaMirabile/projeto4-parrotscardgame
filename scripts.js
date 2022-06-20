fb0e1ea51de0d46e147bac912a97f033d2837033
RafaelaMirabile <rafaelamirabile@gmail.com>
2022-06-20 12:57:59 -0300

    Media Query funcionando

--- scripts.js
+++ scripts.js
@@ -9,23 +9,19 @@ let acertos = 0;
 
 function StartingGame(){
     let jogo= document.querySelector(".cartasContainer");
-    // verificar se o usuario colocou a quantidade correto//
     quantidade = Number(prompt("Com quantas cartas quer jogar? Escolha um número de 4 a 14."));
 
     while( quantidade < 4 ||  quantidade > 14 || quantidade % 2 !== 0 || !quantidade ){
         quantidade = Number(prompt(" Por favor escolha um número de 4 a 14."));
     }
-            // colocar adiconar cartas no array //
             while(cartasNoJogo.length !== quantidade){
                 cartasNoJogo.push(carta,carta);
                 carta ++   
             }
     
             let cartasEmbaralhadas = cartasNoJogo.sort(comparador);
-
-
-    // colocar cartas no tabuleiro//
-    while(i < cartasEmbaralhadas.length){
+    
+            while(i < cartasEmbaralhadas.length){
         jogo.innerHTML += `<div class="carta" onclick="virar(this)">
         <div class="frente face">
             <img src="images/front.png" alt="">
@@ -39,21 +35,14 @@ function StartingGame(){
 
 }
 StartingGame();
-
-
 function virar(cartaClicada){
-
-    console.log(cartaClicada.classList);  
 
     if(cartaClicada.classList.contains("virada") || segundoClick !== undefined){
         primeiroClick = undefined;
         segundoClick= undefined;
     }
-    console.log(cartaClicada , "verifica;'ap");
     jogadas++;
     
-    console.log(jogadas);
-
     cartaClicada.classList.add("virada");
 
     if(primeiroClick === undefined){
@@ -64,7 +53,6 @@ function virar(cartaClicada){
     
     if (primeiroClick.innerHTML === segundoClick.innerHTML){
         acertos+= 2;
-        console.log(acertos, "cartaIgual");
         fimdeJogo();
     }
     else{
@@ -79,8 +67,6 @@ function desvirar(){
     segundoClick.classList.remove("virada");
     primeiroClick = undefined;
     segundoClick= undefined;
-    console.log(primeiroClick, "primeira");
-    console.log(segundoClick,  "segunda");
 }
 
 function fimdeJogo(){
